b1af1ee429f7f1ce6df44fe71c246fd231f406a8
Shaik Muskinvali <110755598+shaik-muskinvali@users.noreply.github.com>
2024-03-18 21:07:13 +0530

    Delete Stetionography directory

--- Stetionography/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String arg[]) {
-        
-    SplashScreenFrame a=new SplashScreenFrame();
-    a.setVisible(true);
-    a.setLocationRelativeTo(null);
-    }
-}
